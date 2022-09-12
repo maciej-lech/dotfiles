ea6f858e410156823f8574700e31e3d3368afec6
Maciej Lech <maciej.lech@mlit.pro>
2022-09-12 16:42:19 +0200

    Update for Ubuntu 22.04

--- zsh/__init__.py
+++ zsh/__init__.py
@@ -7,6 +7,7 @@ from dotfiles import symlink
 def bootstrap(c):
     with c.cd('zsh'):
         symlink(c, 'zshrc', '~/.zshrc')
+        symlink(c, 'zprofile', '~/.zprofile')
         symlink(c, 'aliases.zsh', '~/.zsh/aliases.zsh')
 
         # Antigen
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,7 +2,6 @@
 
 lockmount() {
     encfs -f $HOME/.Lock_encfs $HOME/Lock
-    # fusermount -u Lock
 }
 
 ## Aliases
@@ -21,6 +20,7 @@ alias grepi='grep -i'
 alias grepr='grep -r'
 alias grepir='grep -ir'
 alias findf='find . -name'
+alias a=ack-grep
 
 # Git
 alias g='git'
@@ -37,28 +37,25 @@ alias aptsch='apt search'
 alias aptsho='apt show'
 alias aptpol='apt policy'
 
-alias aptlist='dpkg -l | grep ^ii | cut -d ' ' -f3 | tr '\n' ' ''
+alias aptlist="dpkg -l | grep ^ii | cut -d ' ' -f3 | tr '\n' ' '"
 alias aptman="comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort -u)"
 
-# python
+# Python
 alias pip-upgrade-all='pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U'
 alias pip-uninstall-all='pip uninstall -y -r <(pip freeze)'
 alias pip-uninstall-all-local='pip uninstall -y -r <(pip freeze -l)'
 
 alias pe=pyenv
 
+# Editors
+alias st=subl
+
+# Docker
+alias doc=docker
+alias docc=docker-compose
+
 # Misc
 alias xo='xdg-open'
 alias synchtime='sudo ntpdate pl.pool.ntp.org'
 alias sdh='sudo shutdown -h'
 alias ppt2pdf='libreoffice --headless --invisible --convert-to pdf'
-
-# Editors
-alias st=subl
-
-# Grep
-alias a=ack-grep
-
-# Docker
-alias doc=docker
-alias docc=docker-compose
