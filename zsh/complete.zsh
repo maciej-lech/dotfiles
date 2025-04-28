76883873d30f0d35d54977b55b93bf0dd809731c
Maciej Lech <maciej.lech@mlit.pro>
2025-04-28 21:59:40 +0200

    Remove bash and zsh

--- zsh/complete.zsh
+++
@@ -1,9 +0,0 @@
-## Completion functions
-
-_ssh_hosts_completion() {
-    local cur="${COMP_WORDS[COMP_CWORD]}"
-    local hosts
-	hosts=$(	grep '^Host' ~/.ssh/config ~/.ssh/config.d/* .ssh/generated/config.d/*  2>/dev/null | grep -v '[?*]' | cut -d ' ' -f 2- | tr ' ' '\n' | tr -s '\n' '\n')
-    # Deduplicate hosts and create completion options
-    COMPREPLY=( $(compgen -W "$(echo "$hosts" | sort -u)" -- "$cur") )
-}
