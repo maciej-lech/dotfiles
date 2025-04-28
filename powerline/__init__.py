bfef86800162486bf838f33f00d8b46fff2ab028
Maciej Lech <maciej.lech@mlit.pro>
2025-04-28 21:59:54 +0200

    Remove powerline

--- powerline/__init__.py
+++
@@ -1,9 +0,0 @@
-from invoke import task
-
-from dotfiles import symlink
-
-
-@task(default=True)
-def bootstrap(c):
-    with c.cd('powerline'):
-        symlink(c, 'powerline', '~/.config/powerline')
