529953c12bcb12e1bae44d1c7d0ff6e6f530a2aa
Maciej Lech <maciej.lech@mlit.pro>
2025-10-06 17:43:44 +0200

    Use $dir in fzf commands

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -1,8 +1,8 @@
 if command -q fzf
     fzf --fish | source
 
-    set -gx FZF_CTRL_T_COMMAND 'fd --type f --hidden --strip-cwd-prefix --exclude .git'
-    set -gx FZF_ALT_C_COMMAND 'fd --type d --hidden --strip-cwd-prefix --exclude .git'
+    set -gx FZF_CTRL_T_COMMAND 'fd --type f --hidden --exclude .git . $dir'
+    set -gx FZF_ALT_C_COMMAND 'fd --type d --hidden --exclude .git . $dir'
     set -gx FZF_DEFAULT_COMMAND "$FZF_CTRL_T_COMMAND"
     set -gx FZF_CTRL_T_OPTS '--preview "bat -n --color=always --line-range :500 {}"'
     set -gx FZF_ALT_C_OPTS '--preview "eza --tree --group-directories-first --level 2 --color=always {} | head -200"'
