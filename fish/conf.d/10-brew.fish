76656c31152a83c0a3aa48ea4f932812a9f0362b
Maciej Lech <maciej.lech@mlit.pro>
2025-10-06 18:26:30 +0200

    Add brew command-not-found handler

--- fish/conf.d/10-brew.fish
+++ fish/conf.d/10-brew.fish
@@ -1,3 +1,8 @@
 if command -q brew
+    set HOMEBREW_COMMAND_NOT_FOUND_HANDLER (brew --repository)/Library/Homebrew/command-not-found/handler.fish
+    if test -f $HOMEBREW_COMMAND_NOT_FOUND_HANDLER
+        source $HOMEBREW_COMMAND_NOT_FOUND_HANDLER
+    end
+
     set -p fish_complete_path /home/linuxbrew/.linuxbrew/share/fish/vendor_completions.d
 end
