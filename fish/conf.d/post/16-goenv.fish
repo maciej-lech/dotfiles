1dec5eb28ed1bcb6727a2c8330b2ca1794effea5
Maciej Lech <maciej.lech@mlit.pro>
2025-10-21 17:23:00 +0200

    Fix the order of sourcing conf.d files in fish

--- fish/conf.d/post/16-goenv.fish
+++ fish/conf.d/post/16-goenv.fish
@@ -3,5 +3,5 @@ if test -d $HOME/.goenv && ! set -q IN_NIX_SHELL
     fish_add_path -P $GOENV_ROOT/bin
     goenv init - fish | source
     #fish_add_path -pP $GOROOT/bin
-    fish_add_path -aP $GOPATH/bin
+    fish_add_path -P $GOPATH/bin
 end
--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,11 @@
+# config.fish
+
+# The order of sourcing conf.d is ackward: https://github.com/fish-shell/fish-shell/issues/8553
+# Use use our own custom source loop to be able to specify the order.
+for file in "$__fish_config_dir"/conf.d/post/*.fish
+    source $file
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
