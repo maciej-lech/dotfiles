dea642eeb4ea3f5988586b0812e44b3c8d2e1f5d
Maciej Lech <maciej.lech@mlit.pro>
2025-10-21 17:43:21 +0200

    Update goenv config

--- fish/conf.d/post/16-goenv.fish
+++ fish/conf.d/post/16-goenv.fish
@@ -1,7 +1,6 @@
 if test -d $HOME/.goenv && ! set -q IN_NIX_SHELL
     set -gx GOENV_ROOT $HOME/.goenv
+    set -gx GOENV_PATH_ORDER "front"
     fish_add_path -P $GOENV_ROOT/bin
     goenv init - fish | source
-    #fish_add_path -pP $GOROOT/bin
-    fish_add_path -P $GOPATH/bin
 end
