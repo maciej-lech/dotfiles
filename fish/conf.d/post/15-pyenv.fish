e8bc03ef94914d9251b46cf5243975d01ace3582
Maciej Lech <maciej.lech@mlit.pro>
2026-02-22 18:03:09 +0100

    Remove pyenv and goenv from fish

--- fish/conf.d/post/15-pyenv.fish
+++
@@ -1,8 +0,0 @@
-function fish_pyenv --description "Setup Pyenv"
-    if command -q pyenv && ! set -q IN_NIX_SHELL
-        set -gx PYENV_ROOT $HOME/.pyenv
-        fish_add_path -P $PYENV_ROOT/bin
-        pyenv init - fish | source
-        pyenv virtualenv-init - fish | source
-    end
-end
